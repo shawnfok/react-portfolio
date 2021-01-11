3dceed695ece3b6f317989bd221a395b9ad1ba37
shawnfok <shawnfok@gmail.com>
2021-01-11 08:47:17 -0500

    polished UI

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -9,28 +9,30 @@ import {
 
 export default function SocialFollow() {
   return (
-    <div style={{float: 'left'}} class="social-container">
+    <div style={{float: 'center'}} class="social-container">
       <a
         href="https://www.linkedin.com/in/shawnfok/"
-        className="linkedin social"
+        className="linkedin social mr-1"
       >
         <FontAwesomeIcon icon={faLinkedinIn} size="2x" color='gray'/>
       </a>
       <a
         href="https://vimeo.com/user44481560"
-        className="vimeo"
+        className="vimeo mr-1"
       >
         <FontAwesomeIcon icon={faVimeo} size="2x" color='gray' />
       </a>
-      <a href="https://github.com/shawnfok" className="github social">
+      <a href="https://github.com/shawnfok" 
+        className="github social mr-1"
+        >
         <FontAwesomeIcon icon={faGithub} size="2x" color='gray' />
       </a>
-      <a
+      {/* <a
         href="https://www.instagram.com/"
         className="instagram social"
       >
         <FontAwesomeIcon icon={faInstagram} size="2x" color='gray'/>
-      </a>
+      </a> */}
     </div>
   );
 }
--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -18,7 +18,7 @@ export default class aboutme extends Component {
               src={ProfilePic}
               alt="Generic placeholder"
               fluid
-              thumbnail
+              // thumbnail
             />
             <br />
             <SocialFollow />
